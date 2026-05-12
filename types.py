d2138a36b5b1a7e3f2a660b262a0af8b7989e8b9
dylmcgold <dylanmcgoldrick@gmail.com>
2026-05-11 21:58:49 -0700

    Adding program covering types

---
+++ types.py
@@ -0,0 +1,9 @@
+print(type(123))
+#avogadro's number - the molar constant
+print(type(6.02e23))
+print(type(False))
+print(type(lambda x: x * x))
+print(type({'OR':'Salem','ME':'Augusta','VT':'Montpelier'}))
+print(type(x for x in range(5)))
+print(type(print))
+print(type(float))
