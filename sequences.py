12e47b37f6c141000c0bc7efb3bb7e33dfebcd12
dylmcgold <dylanmcgoldrick@gmail.com>
2026-05-18 12:57:05 -0700

    Added program on sequences

---
+++ sequences.py
@@ -0,0 +1,18 @@
+a=[10,20,30,40,50,60,70,80,90,100]
+print(a[3])
+print(a[3:5])
+print(a[:8])
+print(a[2:])
+print(a[1:7:3])
+print(98 in a)
+print(98 not in a)
+b = [16,32,64]
+print(a + b)
+print(a < b)
+a += b 
+print(a)
+c = [16,32,64]
+print(b == c)
+n = -37
+print(bin(n))
+print(n.bit_length())
