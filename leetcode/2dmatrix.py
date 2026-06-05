34b1e870695a9545623ede57cab1777fb61e17ec
dylmcgold <dylanmcgoldrick@gmail.com>
2026-06-05 14:19:57 -0700

    Adding 2dmatrix.py

---
+++ leetcode/2dmatrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        if not (top <= bot):
+            return False
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target < matrix[row][m]:
+                r = m - 1 
+            elif target > matrix[row][m]:
+                l = m + 1
+            else:
+                return True
+        return False
+
+obj = Solution()
+#using pytest
+def test_searchmatrix():
+    assert obj.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3) == True
+    assert obj.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13) == False
+
