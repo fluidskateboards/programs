92b80f3bc35584cf14f35f3c3064dc0c13a1916b
dylmcgold <dylanmcgoldrick@gmail.com>
2026-06-16 16:41:17 -0700

    adding hof.py

---
+++ classnotes/hof.py
@@ -0,0 +1,10 @@
+
+def is_odd(x): return x % 2 == 1
+def two_x_plus_y(x,y): return x*2 + y
+def square(x): return x * x
+print(is_odd(7))
+print(is_odd(8))
+print(two_x_plus_y(3,1))
+print(two_x_plus_y(4,0))
+print(square(64))
+print(square(8))
