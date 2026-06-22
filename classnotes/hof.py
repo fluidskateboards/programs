d49bacd37f5805a2441ffe912db77a90cd39d68f
dylmcgold <dylanmcgoldrick@gmail.com>
2026-06-22 13:09:48 -0700

    Added to hof.py

--- classnotes/hof.py
+++ classnotes/hof.py
@@ -8,3 +8,14 @@ print(two_x_plus_y(3,1))
 print(two_x_plus_y(4,0))
 print(square(64))
 print(square(8))
+addSix = lambda x: x + 6
+print(addSix(6))
+def compose (f, g):
+    return lambda x: f(g(x))
+def twice(f):
+    return compose(f, f)
+add_six_then_square = compose(square, addSix)
+add_twelve = twice(addSix)
+assert add_six_then_square(9) == 225
+assert twice(square)(3) == 81
+assert add_twelve(100) == 112
